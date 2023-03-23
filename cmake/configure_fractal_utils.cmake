1248bd521ae0670f08a2d2ea5bfd1cad8257ee11
虫 <tokinobug@163.com>
2023-03-23 21:14:05 +0800

    switch to fractal_utils v2.1.0

--- cmake/configure_fractal_utils.cmake
+++ cmake/configure_fractal_utils.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(fractal_utils
     GIT_REPOSITORY https://github.com/ToKiNoBug/FractalUtils.git
-    GIT_TAG main
+    GIT_TAG v2.1.0
     OVERRIDE_FIND_PACKAGE
 
     # URL https://github.com/ToKiNoBug/FractalUtils/archive/refs/tags/v2.0.1.tar.gz
