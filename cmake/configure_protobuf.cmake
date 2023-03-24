701bd830ac7ae85b7bf30d1c1096ae320c5fd863
虫 <tokinobug@163.com>
2023-03-24 12:53:17 +0800

    remove protobuf dependency

--- cmake/configure_protobuf.cmake
+++
@@ -1,9 +0,0 @@
-return()
-include(FetchContent)
-FetchContent_Declare(protobuf
-    GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
-    GIT_TAG v22.2
-    OVERRIDE_FIND_PACKAGE
-)
-
-FetchContent_MakeAvailable(protobuf)
